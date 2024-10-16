c39fa7c5a2bfd474afb2291104db6691068a2544
Apurbo Roy <apurboroy7077@gmail.com>
2024-10-16 23:08:07 +0600

    updated multiplayer game function

--- src/data/routes-addresses/addresses.ts
+++ src/data/routes-addresses/addresses.ts
@@ -2,8 +2,10 @@ export const MULTIPLAYER_GAME_PAGE_ADDRESS = "/multiplayer-game";
 
 // LOCALHOST ADDRESS
 //
-export const NORMAL_SERVER_ADDRESS = "http://localhost:5006";
-export const SOCKET_ADDRESS = "http://localhost:5006";
+export const NORMAL_SERVER_ADDRESS =
+  "https://quiztifyar7-backend-2-express-socket.onrender.com";
+export const SOCKET_ADDRESS =
+  "https://quiztifyar7-backend-2-express-socket.onrender.com";
 //
 //
 //
